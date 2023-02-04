ce472ee82f9850104f83503b0d635ae067c53b41
Arhises <roma1039@gmail.com>
2023-02-04 18:16:06 +0200

    Question one added

---
+++ Question_01/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти кубы чисел от 1 до N
+
+Console.WriteLine("Enter number: ");
+int num = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < num; i++)
+{
+    Console.WriteLine($"{i}^2 = {i*i}");
+}
+
